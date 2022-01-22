96d883fa7b094e88d48b5f88a2efd831f67dc34a
Yana Kopyshchyk <yanakopyshchyk@MBP-Yana.lan>
2022-01-22 19:57:50 +0200

    Added solution for 02-event-loop, 01-events-order

--- 02-event-loop/01-events-order/index.js
+++ 02-event-loop/01-events-order/index.js
@@ -1,18 +1,18 @@
-const intervalId = setInterval(() => {
+const intervalId = setInterval(() => { // 1
   console.log('James');
 }, 10);
 
-setTimeout(() => {
+setTimeout(() => { // 2
   const promise = new Promise((resolve) => {
     console.log('Richard');
-    resolve('Robert');
+    resolve('Robert'); // 3
   });
 
   promise
-      .then((value) => {
+      .then((value) => { // 4
         console.log(value);
 
-        setTimeout(() => {
+        setTimeout(() => { // 5
           console.log('Michael');
 
           clearInterval(intervalId);
